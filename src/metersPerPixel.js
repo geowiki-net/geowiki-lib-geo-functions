b07c90f5aaeb56bb813a83d2c97d017be629431b
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2026-03-27 22:01:11 +0100

    metersPerPixel: fix code

--- src/metersPerPixel.js
+++ src/metersPerPixel.js
@@ -1,14 +1,21 @@
-module.exports = function metersPerPixel (map = null) {
-  if (!map && global.map && global.map.getCenter && typeof global.map.getCenter === 'function') {
-    const center = global.map.getCenter()
-    map = {
-      zoom: global.map.getZoom,
-      center: { lat: center.lat, lon: center.lng }
-    }
+module.exports = function metersPerPixel (map = {}) {
+  if (!map) {
+    map = {}
+  }
+
+  if (!map.zoom && global.map && global.map.getZoom && typeof global.map.getZoom === 'function') {
+    map.zoom = global.map.getZoom()
   } else {
     return 1
   }
 
-  map.metersPerPixel = 40075016.686 * Math.abs(Math.cos(map.lat / 180 * Math.PI)) / Math.pow(2, map.zoom + 8)
+  if (!map.center && global.map && global.map.getCenter && typeof global.map.getCenter === 'function') {
+    const center = global.map.getCenter()
+    map.center = { lat: center.lat, lon: center.lng }
+  } else {
+    return 1
+  }
+
+  map.metersPerPixel = 40075016.686 * Math.abs(Math.cos(map.center.lat / 180 * Math.PI)) / Math.pow(2, map.zoom + 8)
   return map.metersPerPixel
 }
